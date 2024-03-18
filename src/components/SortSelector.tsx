69e40f81bd3489c2522f916024c33de1a345ca4c
Salik-07 <salikhussain41@gmail.com>
2024-03-19 01:24:06 +0500

    Add sort selector component

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,34 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+// import usePlatforms, { Platform } from "../hooks/usePlatforms";
+
+// interface Props {
+//   selectedPlatform: Platform | null;
+//   onSelectPlatform: (platform: Platform) => void;
+// }
+
+const SortSelector = () =>
+  // { onSelectPlatform, selectedPlatform }: Props
+  {
+    //   const { data, error } = usePlatforms();
+
+    //   if (error) return null;
+
+    return (
+      <Menu>
+        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+          Order by: Relevance
+        </MenuButton>
+        <MenuList>
+          <MenuItem>Relevance</MenuItem>
+          <MenuItem>Date added</MenuItem>
+          <MenuItem>Name</MenuItem>
+          <MenuItem>Release date</MenuItem>
+          <MenuItem>Popularity</MenuItem>
+          <MenuItem>Average rating</MenuItem>
+        </MenuList>
+      </Menu>
+    );
+  };
+
+export default SortSelector;
