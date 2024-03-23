811a5ad13df9143e3e2c2df676b948d7116a1624
Salik-07 <salikhussain41@gmail.com>
2024-03-23 20:11:20 +0500

    Add game attributes component

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,12 +1,16 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
   description_raw: string;
   background_image: string;
+  genres: Genre[];
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
+  publishers: Publisher[];
   slug: string;
 }
